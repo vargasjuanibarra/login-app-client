9019ca4ddd52523a4eb1c1126fd6e1b94b711a59
vargasjuanibarra <78344342+vargasjuanibarra@users.noreply.github.com>
2021-04-07 18:51:29 +0800

    changed GET app.js /.*/

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname + "/dist/"));
-app.get('/.*/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + "/dist/index.html");
 });
 
